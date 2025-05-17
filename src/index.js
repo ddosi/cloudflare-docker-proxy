51f975433fb02df7313c9249ca27d15f56f0ad2d
hack <48151487+ddosi@users.noreply.github.com>
2025-05-17 19:05:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.closeai.me": dockerHub,
-  "quay.closeai.me": "https://quay.io",
-  "gcr.closeai.me": "https://gcr.io",
-  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
-  "k8s.closeai.me": "https://registry.k8s.io",
-  "ghcr.closeai.me": "https://ghcr.io",
-  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
+  "docker.cdn.2080888.xyz": dockerHub,
+  "quay.cdn.2080888.xyz": "https://quay.io",
+  "gcr.cdn.2080888.xyz": "https://gcr.io",
+  "k8s-gcr.cdn.2080888.xyz": "https://k8s.gcr.io",
+  "k8s.cdn.2080888.xyz": "https://registry.k8s.io",
+  "ghcr.cdn.2080888.xyz": "https://ghcr.io",
+  "cloudsmith.cdn.2080888.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.closeai.me": dockerHub,
+  "docker-staging.cdn.2080888.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
